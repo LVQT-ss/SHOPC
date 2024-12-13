fd0a3ac2827a042d4253182903176fc2a6e84023
LVQT <levietquocthinh@gmail.com>
2024-12-13 19:09:05 +0700

    base folder

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,33 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Our Koi Care Service
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to our Koi Care Service! This platform is dedicated to helping you
+              take care of your beloved koi fish by providing expert advice and comprehensive
+              tools for managing your ponds, monitoring water conditions, and ensuring the
+              health of your koi.
+            </p>
+
+            <p>
+              We offer weekly insights, articles, and tips on koi care, pond maintenance,
+              and water quality management. Our goal is to assist koi enthusiasts in creating
+              a safe and healthy environment for their fish, promoting their well-being and longevity.
+            </p>
+
+            <p>
+              Join our community of koi fish lovers! You can comment on our posts, ask questions, and share
+              your experiences. Whether you are a beginner or a seasoned koi keeper, we are here to support
+              you on your journey.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1000, // Đặt giới hạn cảnh báo thành 1000KB (1MB)
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+})
