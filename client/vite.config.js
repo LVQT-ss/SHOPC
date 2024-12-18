ee9d19b709cea4655674418b45fc51b81b357bbc
LVQT <levietquocthinh@gmail.com>
2024-12-19 02:15:36 +0700

    vercel config deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // Adjust the base depending on where the client will be served ("/" for root).
   build: {
-    chunkSizeWarningLimit: 1000, // Đặt giới hạn cảnh báo thành 1000KB (1MB)
+    chunkSizeWarningLimit: 1000, // Set warning limit to 1000KB (1MB)
   },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+        target: 'https://shopc-5tfn.onrender.com', // Proxy requests to the deployed backend
+        changeOrigin: true, // Ensure the origin of the request matches the target
+        secure: true, // Use secure HTTPS for the backend
       },
     },
   },
   plugins: [react()],
-})
+});
