83766c65afde7fbf310dc967c5099a06e02587ed
LVQT <levietquocthinh@gmail.com>
2024-12-23 23:41:09 +0700

    signInfunction on FE

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -7,6 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
+import { login } from "../Utils/ApiFunctions";
 import OAuth from "../components/OAuth";
 
 export default function SignIn() {
@@ -14,9 +15,11 @@ export default function SignIn() {
   const { loading, error: errorMessage } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
@@ -24,28 +27,20 @@ export default function SignIn() {
     }
     try {
       dispatch(signInStart());
-      const res = await fetch("/api/auth/signin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+      const data = await login({
+        username: formData.email,
+        password: formData.password,
       });
-      const data = await res.json();
-      if (data.success === false) {
-        dispatch(signInFailure(data.message));
-      }
-
-      if (res.ok) {
-        dispatch(signInSuccess(data));
-        navigate("/");
-      }
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
       dispatch(signInFailure(error.message));
     }
   };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
@@ -55,16 +50,15 @@ export default function SignIn() {
           </Link>
           <p className="text-sm mt-5">ĐĂNG NHẬP</p>
         </div>
-        {/* right */}
 
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value="Your email" />
+              <Label value="Username" />
               <TextInput
-                type="email"
-                placeholder="name@company.com"
-                id="email"
+                type="text"
+                placeholder="Tên đăng nhập hoặc số điện thoại"
+                id="text"
                 onChange={handleChange}
               />
             </div>
