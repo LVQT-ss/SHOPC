9af6acc777f4d74704cccef1f1e0458492bb837c
LVQT <levietquocthinh@gmail.com>
2025-01-06 21:38:48 +0700

    test bug UI router

--- client/src/router/router.jsx
+++ client/src/router/router.jsx
@@ -34,7 +34,7 @@ const ErrorBoundary = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold">Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Sorry, an unexpected error has occurred. 000 </p>
     </div>
   );
 };
