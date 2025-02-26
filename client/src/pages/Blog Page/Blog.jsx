378f5cd9dbc35a718c9d34dae20c85d46533207f
QuyDuong162 <duonghqse171776@fpt.edu.vn>
2025-02-26 21:06:45 +0700

    fix margin

--- client/src/pages/Blog Page/Blog.jsx
+++ client/src/pages/Blog Page/Blog.jsx
@@ -47,7 +47,7 @@ const BlogsLeft = [
 
 export default function Blog() {
   return (
-    <div className="max-w-6xl mx-auto mt-8 p-4">
+    <div className="max-w-6xl mx-auto p-4">
       <BlogHeader />
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
